56b1a79fbf856e2aadc6eb705958d7d09bd187fa
Mahdi Fada <mahdifada.eddini@live.com>
2026-06-20 15:51:21 +0330

    dd start up data as scriptable object. add assembly reference

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+  version : 0.1.1
+  add start up data as scriptable object. add assembly reference
+
   version : 0.1.0
   initail version, fork from main ad-trace unity sdk in 6/20/2026, clean up and setup package
---
+++ Runtime/AdTraceStartupData.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace io.adtrace.sdk
+{
+    [CreateAssetMenu(fileName = "AdTraceData", menuName = "PackageData/Create AdTraceData")]
+    public class AdTraceStartupData : ScriptableObject
+    {
+        [SerializeField]
+        private bool _isEnable;
+        [SerializeField]
+        private bool _eventBuffering = false;
+        [SerializeField]
+        private bool _sendInBackground = false;
+        [SerializeField]
+        private bool _launchDeferredDeeplink = true;
+        [SerializeField]
+        private string _appToken;
+        [SerializeField]
+        private AdTraceLogLevel _logLevel;
+        [SerializeField]
+        private AdTraceEnvironment _adTraceEnvironment;
+
+        public bool IsEnable => _isEnable;
+        public bool EventBuffering => _eventBuffering;
+        public bool SendInBackground => _sendInBackground;
+        public bool LaunchDeferredDeeplink => _launchDeferredDeeplink;
+        public string AppToken => _appToken;
+        public AdTraceLogLevel LogLevel => _logLevel;
+        public AdTraceEnvironment AdTraceEnvironment => _adTraceEnvironment;
+    }
+}
